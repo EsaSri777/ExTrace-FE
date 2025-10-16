928d8c6c5e3518495e16c7b49d9f9b4ddfe26447
esasri <Esasri2007@gmail.com>
2025-10-16 12:30:13 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000/api',
-    googleClientId: '913333401285-22lu17p41dh3rse1rm3g35nc36bh7vmr.apps.googleusercontent.com'
+  production: false,
+  apiUrl: 'https://extrace-be.onrender.com/api',
+  googleClientId: '286951952181-780vc6oqee4jpahm8qco8u1o1ki531c8.apps.googleusercontent.com',
 };
